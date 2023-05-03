281411cdb5d9ffaba60faa8fb50c1886ce469f48
kingkonu <81903690+kingkonu@users.noreply.github.com>
2023-05-04 01:39:44 +0300

    Start to change Button text

--- SpringApp/ViewControllers/ViewController.swift
+++ SpringApp/ViewControllers/ViewController.swift
@@ -13,15 +13,24 @@ final class ViewController: UIViewController {
     private let springs = Spring.getAnimation()
 
     @IBOutlet weak var springAnimationView: SpringView!
-
+    @IBOutlet weak var animationNameLabel: UILabel!
+    @IBOutlet weak var curveNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        springAnimationView.animation = "pop"
+        animationNameLabel.text = "Pop"
+        springAnimationView.curve = "easeIn"
+        curveNameLabel.text = "easeIn"
     }
 
     @IBAction func springButton(_ sender: SpringButton) {
 
         for spring in springs.shuffled() {
+            animationNameLabel.text = spring.preset
+            curveNameLabel.text = spring.curve
+            sender.setTitle(spring.preset, for: .normal)
+
             springAnimationView.animation = spring.preset
             springAnimationView.curve = spring.curve
             springAnimationView.animate()
